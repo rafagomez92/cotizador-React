3b8f967b4d3a106c6160c7890605317679338427
rafagomez92 <rafael_gomez92@hotmail.com>
2020-11-11 00:45:13 -0600

    app de cotizacion

---
+++ src/Helper.js
@@ -0,0 +1,40 @@
+export function obtenerDiferenciaYear(year) {
+    return new Date().getFullYear() - year;
+    
+}
+
+// Calcular el total a pagar segun la marca
+
+export function calcularMarca(marca) {
+    let incremento;
+    switch(marca) {
+        case "europeo":
+            incremento = 1.30;
+            break; 
+        case "americano":
+            incremento = 1.15;
+            break; 
+        case "asiatico":
+            incremento = 1.05;
+            break;         
+
+        default:
+            break;
+
+
+    }
+    return incremento;
+    
+}
+
+// Calcular el tipo de seguro
+export function obtenerPlan( plan) {
+    return (plan === 'basico' ? 1.28 : 1.50);
+
+}
+
+// Muestra la primer letra mayuscula
+export function primerMayuscula(texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+
+}
